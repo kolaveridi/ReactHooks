91dcaacba1896ff4c68c4b98b394348f7db414b3
APPLE <apple@APPLEs-MacBook-Air-2.local>
2019-07-20 18:35:42 +0530

    basic counter in hooks

---
+++ src/AppFunction.js
@@ -0,0 +1,18 @@
+import React ,{useState} from 'react';
+
+const App =() =>{
+
+    const [count,setCount] = useState(0);
+
+    const  incrementCount =()=>{
+         setCount(count+1);
+    }
+
+    return (
+        <button  onClick={incrementCount}>
+             {count}
+            </button>
+    );
+}
+
+export default App;
