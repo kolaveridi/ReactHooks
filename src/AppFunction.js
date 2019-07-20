148b4dff71072bae094bffab2d25ccfb8dfc20a5
APPLE <apple@APPLEs-MacBook-Air-2.local>
2019-07-20 20:18:33 +0530

    useEffect react hooks

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,9 +1,10 @@
-import React ,{useState} from 'react';
+import React ,{useState ,useEffect} from 'react';
 
 const App =() =>{
 
     const [count,setCount] = useState(0);
     const [isOn,setIsOn] = useState(false);
+    const [mousePosition,setMousePosition] = useState({x:null,y:null});
 
     const  incrementCount =()=>{
          setCount(prevCount => prevCount+1);
@@ -12,11 +13,35 @@ const App =() =>{
     const toggleLight =() =>{
         setIsOn(prevIsOn => !prevIsOn)
     }
+    useEffect(()=>{
+       document.title =`You have clicked ${count} times` ; 
+       window.addEventListener('mousemove',handleMouseMove)
+       return ()=>{
+        window.removeEventListener('mousemove',handleMouseMove)
+    }
+       } ,[ ])
+
+    const handleMouseMove = event =>{
+        setMousePosition({
+            x:event.pageX,
+            y:event.pageY
+        })
+    }
+        
 
     return (
+        <div>
         <button  onClick={incrementCount}>
              {count}
+
             </button>
+            <div>
+            {mousePosition.x}
+            <br></br>
+            {mousePosition.y}
+            </div>
+
+            </div>
     );
 }
 
